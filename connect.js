ddd11e9fb96de1b26bb7004ec3f65689ac1d99b3
Likhith kumar <likhithmattapalli@gmail.com>
2022-12-04 11:40:01 +0530

    firstt commit

--- connect.js
+++ connect.js
@@ -4,7 +4,7 @@ module.exports={
     selectedDb:{},
     async connect(){
         try {
-            const client=await MongoClient.connect('mongodb+srv://likhithkumar:BySVeto8CV6UtFzb@cluster0.3xbqzjp.mongodb.net/?retryWrites=true&w=majority')
+            const client=await MongoClient.connect(process.env.MONGO_URL)
             this.selectedDb=client.db('Stackoverflow')
         } catch (error) {
             console.log(error)
